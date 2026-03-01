478422c7c7eb72e053c27e875ab3ecaa71098f44
PeiyingMango <hpy.peiying@gmail.com>
2026-03-02 00:16:41 +0800

    Create node.js

---
+++ node.js
@@ -0,0 +1,15 @@
+{
+  "name": "urlscan-backend",
+  "version": "1.0.0",
+  "description": "Backend proxy for urlscan.io API to avoid exposing API key",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "node-fetch": "^3.3.2",
+    "cors": "^2.8.5"
+  },
+  "type": "module"
+}
